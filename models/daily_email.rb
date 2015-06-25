79c52c2fc2579a1cf2243a20f0ba9ecf326ac597
aditi-gupta <aditi100gupta@gmail.com>
2015-06-25 18:16:25 +0000

    YAY NYT one article working in email.

--- models/daily_email.rb
+++ models/daily_email.rb
@@ -15,32 +15,25 @@ class DailyEmail
     
     url = open('http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8d4f4955fc1ec5ba9bb216928505e58b:18:71651992')
 
-
       response_hash = JSON.load(open(url))
-      @headline = response_hash 
-    binding.pry
-    
+      @url = response_hash["response"]["docs"][0]["web_url"] 
+      @headline = response_hash["response"]["docs"][0]["headline"]["main"]
+      @snippet = response_hash["response"]["docs"][0]["snippet"]    
     
     @emails.each do |email|
       RestClient.post "https://api:key-3f0bf97cdc41cba9bb58712d34992b25"\
     "@api.mailgun.net/v3/sandboxd0e86587ad9f4ec8845133e378927022.mailgun.org/messages",
       :from => "Mailgun Sandbox <postmaster@sandboxd0e86587ad9f4ec8845133e378927022.mailgun.org>",
       :to => email, 
-      :subject => "Your Daily Update",
-      :html => "<html><img src=#{@gif}> #{@headline} <h3>This is your daily stuff. Brought to you by the Daily Update.</h3></html>" 
-      #:text =>
+      :subject => "Please say this has all the NYT stuff...",
+      :html => "<html><img src=#{@gif}> 
+<h3>#{@headline}</h3> 
+<h6>#{@snippet}</h6> 
+View the full article here:#{@url} 
+<h3>This is your daily stuff. Brought to you by the Daily Update.</h3></html>" 
       
-    end 
-    
-    
+    end   
   end
-  
-#   def api_call
-#     @url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8d4f4955fc1ec5ba9bb216928505e58b:18:71651992"
-#       response_hash = JSON.load(open(@url))
-#       @headline = response_hash
-   
- 
 end
 
 test_email = DailyEmail.new
