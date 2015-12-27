72ed483e853a4ec9c1b261c6623ff72f00ff8fff
aditi-gupta <aditi100gupta@gmail.com>
2015-12-26 23:09:16 -0500

    changed api (mailgun to daily update)

--- models/unsubscribe.rb
+++ models/unsubscribe.rb
@@ -9,9 +9,9 @@ class Unsubscriber
   end
   def send_unsubscriber_message
     @our_emails.each do |my_email|
-      RestClient.post "https://api:key-b3c3fcdb51c644397a7748c033c1c804"\
-    "@api.mailgun.net/v3/sandboxd0e86587ad9f4ec8845133e378927022.mailgun.org/messages",
-      :from => "Mailgun Sandbox <postmaster@sandboxd0e86587ad9f4ec8845133e378927022.mailgun.org>",
+      RestClient.post "https://api:key-a16cd16a87d93b2c72100c8dfc9a86a3"\
+    "@api.mailgun.net/v3/the-daily-update.com/messages",
+      :from => "The Daily Update <postmaster@the-daily-update.com>",
       :to => my_email, 
       :subject => "Unsubscriber",
       :text => "The user with the email #{unsubscriber_email} would like to unsubscribe from the Daily Update service. Please take them out of the array of subscribers."
